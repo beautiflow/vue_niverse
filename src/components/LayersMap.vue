ed73678a106e8f860fc96723b46d79bd36c96da5
unknown <1114ny@naver.com>
2025-07-30 11:27:22 +0900

    add WFS map

--- src/components/LayersMap.vue
+++ src/components/LayersMap.vue
@@ -7,10 +7,109 @@ import { ref, onMounted } from 'vue';
 import 'ol/ol.css';
 import ImageLayer from 'ol/layer/Image.js';
 import ImageWMS from 'ol/source/ImageWMS.js';
+import GeoJSON from 'ol/format/GeoJSON.js';
+import VectorLayer from 'ol/layer/Vector.js';
+import {bbox as bboxStrategy} from 'ol/loadingstrategy.js';
+import ImageTile from 'ol/source/ImageTile.js';
+import VectorSource from 'ol/source/Vector.js';
 
 let map;
 let wmsLayer = null;
+let raster = null;
+let vector = null;
 const showWMS = ref(false); 
+const showWFS = ref(false); 
+
+// 기본 지도
+const defaultMap = new View({
+  center: fromLonLat([0, 0]), 
+  zoom: 2,
+});
+
+// WMS 지도
+const onWMS = () => {
+  showWMS.value = !showWMS.value; 
+  if(showWMS.value){
+    wmsLayer = new ImageLayer({
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: new ImageWMS({
+      url: 'https://ahocevar.com/geoserver/wms',
+      params: { 'LAYERS': 'topp:states' },
+      ratio: 1,
+      serverType: 'geoserver',
+    }),
+  });
+   const newView = new View({
+      center: [-10997148, 4569099],
+      zoom: 4,
+    });
+    map.setView(newView);
+    map.addLayer(wmsLayer);
+  }else{
+    map.removeLayer(wmsLayer);
+    map.setView(defaultMap);
+    wmsLayer = null;
+  }
+};
+
+// WFS 지도
+const onWFS = () => {
+  showWFS.value = !showWFS.value; 
+  if(showWFS.value){
+    map.addLayer(raster);
+    map.addLayer(vector);
+    const newView = new View({
+      center: [-8908887.277395891, 5381918.072437216],
+      maxZoom: 19,
+      zoom: 12,
+    });
+    map.setView(newView);
+  }
+  else{
+    map.removeLayer(raster);
+    map.removeLayer(vector);
+    map.setView(defaultMap);
+  }
+}
+
+const vectorSource = new VectorSource({
+  format: new GeoJSON(),
+  url: function (extent) {
+    return (
+      'https://ahocevar.com/geoserver/wfs?service=WFS&' +
+      'version=1.1.0&request=GetFeature&typename=osm:water_areas&' +
+      'outputFormat=application/json&srsname=EPSG:3857&' +
+      'bbox=' +
+      extent.join(',') +
+      ',EPSG:3857'
+    );
+  },
+  strategy: bboxStrategy,
+});
+
+ vector = new VectorLayer({
+  source: vectorSource,
+  style: {
+    'stroke-width': 0.75,
+    'stroke-color': 'white',
+    'fill-color': 'rgba(100,100,100,0.25)',
+  },
+});
+
+const key = import.meta.env.VITE_MAP_TILER_KEY;
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
+ raster = new TileLayer({
+  source: new ImageTile({
+    attributions: attributions,
+    url: 'https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=' + key,
+    tileSize: 512,
+    maxZoom: 20,
+  }),
+});
+
 
 onMounted (() => {
  map =  new Map({
@@ -27,40 +126,20 @@ onMounted (() => {
   });
 });
 
-// WMS 지도
-const onWMS = () => {
-  showWMS.value = !showWMS.value; 
-  if(showWMS.value){
-    console.log("showWMS.value = ", showWMS.value);  
-    wmsLayer = new ImageLayer({
-    extent: [-13884991, 2870341, -7455066, 6338219],
-    source: new ImageWMS({
-      url: 'https://ahocevar.com/geoserver/wms',
-      params: { 'LAYERS': 'topp:states' },
-      ratio: 1,
-      serverType: 'geoserver',
-    }),
-  });
-  map.addLayer(wmsLayer);
-  console.log(map.getLayers().getArray().includes(wmsLayer));
-  }else{
-    map.removeLayer(wmsLayer);
-    console.log(map.getLayers().getArray().includes(wmsLayer));
-    wmsLayer = null;
-  }
-};
-
 </script>
 
 <template>
+<div>
   <button @click="onWMS" class="btn btn-success mb-2">    
-    {{ showWMS ? 'Off WMS' : 'On WMS' }}
-  </button>
-
+      {{ showWMS ? 'Off WMS' : 'On WMS' }}
+    </button>
+  <button @click="onWFS" class="btn btn-primary ms-2 mb-2 ">    
+      {{ showWFS ? 'Off WFS' : 'On WFS' }}
+    </button>
+  </div>
   <div id='map-container'></div>
   <div>
-
-  </div>
+</div>
 </template>
 
 
