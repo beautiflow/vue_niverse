3f91dba35f179e11691809184e17e25eb6d79183
nayeon <1114ny@naver.com>
2025-09-02 14:08:49 +0900

    feat: add vue-datepicker component

---
+++ src/components/datePickerComponent.vue
@@ -0,0 +1,55 @@
+<script setup>
+import '@vuepic/vue-datepicker/dist/main.css'
+import Datepicker from "@vuepic/vue-datepicker";
+import { ref, computed, watch } from 'vue';
+
+let pickerDate = ref();
+
+let props = defineProps({
+  label: { Type: String, default: '' },
+  date: [String, Date, null],
+  minDate: {type: Date, default: null},
+  maxDate: {type: Date, default: null},
+  placeholder: { type: String },
+  enableTimePicker: { type: Boolean, default: false},
+  alwaysClearable: {type: Boolean, default: true},
+  showYearFirst: {type: Boolean, default: true},
+  format: {type: String, default: 'yyyy-MM-dd'},
+  disabled: { type: Boolean, default: false },
+  readonly: { type: Boolean, default: false },
+  selectText: { type: String, default: 'Select' },
+  cancelText: { type: String, default: 'Cancel' },
+});
+let emits = defineEmits(['update:modelValue']);
+
+
+const compuedModelType = computed(() => {
+  return props.format;
+});
+
+
+watch(pickerDate, (newValue) => {
+  emits('update:modelValue', newValue);
+});
+</script>
+
+<template>
+  <label>{{ label }}</label>
+
+  <Datepicker
+    v-model="pickerDate"
+    :placeholder="placeholder"
+    locale="ko"
+    :model-type="compuedModelType"
+    :year-first="showYearFirst"
+    :enable-time-picker="enableTimePicker"
+    :always-clearable="alwaysClearable"
+    :min-date="props.minDate"
+    :format="format"
+    :max-date="props.maxDate"
+    :disabled="disabled"
+    :readonly="readonly"
+    :select-text="selectText"
+    :cancel-text="cancelText"
+  />
+</template>
--- src/views/DatePicker.vue
+++ src/views/DatePicker.vue
@@ -1,28 +1,9 @@
 <script setup>
 import SideNavbar from '@/components/SideNavbar.vue';
-import Datepicker from '@vuepic/vue-datepicker'
-import '@vuepic/vue-datepicker/dist/main.css'
-import { ref, watch } from 'vue';
+import DatepickerComponent from '@/components/datePickerComponent.vue';
+import { ref } from 'vue';
 
-
-const props = defineProps({
-  date: String,
-  minDate: Boolean,
-  maxDate: Boolean,
-});
-
-const date = ref(props.date);
-const minDate = ref(props.minDate ? new Date() : null);
-const maxDate = ref(props.maxDate ? new Date() : null);
-
-// onMounted(() => {
-//   const startDate = new Date();
-//   const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
-// })
-
-watch(() => props.date, (newVal) => {
-  date.value = newVal;
-})
+const pickerDate = ref(null);
 
 </script>
 
@@ -33,21 +14,12 @@ watch(() => props.date, (newVal) => {
   </h1>
 
   <div>
-    <Datepicker
-      v-model="date"
-      placeholder="Select Date"
-      locale="ko"
-      format="yyyy-MM-dd"
-      year-first
-      :enable-time-picker="false"
-      :always-clearable="true"
-      :min-date="minDate"
-      :max-date="maxDate"
+    <DatepickerComponent
+      label="등록일시"
+      v-model="pickerDate"
+      placeholder="Select date"
+      format="yyyy년 MM월 dd일"
     />
-    <p>선택한 날짜: {{ date }}</p>
+    <p>선택한 날짜: {{ pickerDate }}</p>
   </div>
 </template>
-
-<style scoped>
-
-</style>
